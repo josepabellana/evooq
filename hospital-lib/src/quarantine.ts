4e9ecda817c31b1ed3e22c673caf5db7934a3a47
Josep Abellana <josep.abe.pu@gmail.com>
2023-01-29 23:50:06 +0100

    x

--- hospital-lib/src/quarantine.ts
+++ hospital-lib/src/quarantine.ts
@@ -4,6 +4,7 @@ export class Quarantine {
 
     private static readonly NOT_IMPLEMENTED_MESSAGE = 'Work, work.';
     patients:PatientsRegister = {};
+    actionsPatients:any = {};
     drugs:Array<string> = [];
     fatal:Boolean = false;
     constructor(patients: PatientsRegister) {
@@ -16,28 +17,62 @@ export class Quarantine {
     }
 
     public wait40Days(): void {
+        this.calculateActions();
+        let newPatients = {};
         for(let groupPatient in this.patients){
-            switch(groupPatient){
-                case 'D':
-                    if(!this.drugs.includes('I')){
-                        this.patients['X']+=this.patients['D'];
-                        this.patients['D']-=this.patients['D'];
-                    }
+            switch(this.actionsPatients[groupPatient]){
+                case 'Dead':
+                    this.patients['X']+=this.patients[groupPatient];
+                    this.patients[groupPatient]-=this.patients[groupPatient];
                     break;
-                case 'F':
-                    if(this.drugs.includes('As') || this.drugs.includes('P')){
-                        this.patients['H'] += this.patients['F'];
-                        this.patients['F'] -= this.patients['F'];
-                    }
-                case 'T':
-                    if(this.drugs.includes('An')){
-                        this.patients['H'] += this.patients['T'];
-                        this.patients['T'] -= this.patients['T'];
-                    }
+                case 'Cured':
+                    this.patients['H'] += this.patients[groupPatient];
+                    this.patients[groupPatient] -= this.patients[groupPatient]; 
+                    break;
+                case 'Remains':
+                   //nothing to do
+                    break;
+                case 'Fever':
+                    this.patients['F'] += this.patients[groupPatient];
+                    this.patients[groupPatient] -= this.patients[groupPatient]; 
+                    break;
             }
         }
     }
     public report(): PatientsRegister {
         return this.patients;
+    }
+    public calculateActions(){
+        //action can be -> 
+        // 1. Dead
+        // 2. Fever
+        // 3. Remains
+        // 4. Cured
+        
+        for(let groupPatient in this.patients){
+            if(this.drugs.includes('I') && this.drugs.includes('As')){
+                this.actionsPatients[groupPatient] = 'Dead';
+            }else{
+                this.actionsPatients[groupPatient] = 'Remains';
+                switch(groupPatient){
+                    case 'D':
+                        if(!this.drugs.includes('I')){
+                            this.actionsPatients[groupPatient] = 'Dead'
+                        }
+                        break;
+                    case 'F':
+                        if(this.drugs.includes('As') || this.drugs.includes('P')){
+                            this.actionsPatients[groupPatient] = 'Fever'
+                        }
+                        break;
+                    case 'T':
+                        if(this.drugs.includes('An')){
+                            this.actionsPatients[groupPatient] = 'Cured'
+                        }
+                        break;
+                }
+            }
+
+        }
     }
 }
